c5ff01d11195d3a2213ee9f5ded70d681c0a32e7
Kawangustavo <106815992+Kawangustavo@users.noreply.github.com>
2022-10-28 17:57:25 -0300

    bruh

--- javaScript/script.js
+++ javaScript/script.js
@@ -14,8 +14,6 @@ $(".slides").slick({
   centerMode: true,
   centerPadding: "60px",
   slidesToShow: 3,
-  autoplay: true,
-  autoplaySpeed: 2000,
   prevArrow: $("#arrow-prev"),
   nextArrow: $("#arrow-next"),
   responsive: [
